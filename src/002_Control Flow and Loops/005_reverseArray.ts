ce0192ed7ed452df45771d9b843ed3d17bdaf347
MrSapthagiri <sapthagiri04032003@gmail.com>
2026-06-09 14:33:48 +0530

    refactor: Uncomment array reversal logic in reverseArray file for clarity

--- src/002_Control Flow and Loops/005_reverseArray.ts
+++ src/002_Control Flow and Loops/005_reverseArray.ts
@@ -1,14 +1,10 @@
-// export {};
+export {};
 
-// const arr: number[] = [1, 2, 3, 4, 5];
-// let reversed: number[] = [];
-// for (let i = arr.length - 1; i >= 0; i--) {
-//     reversed.push(arr[i]);
-// }
-// console.log(reversed);
+const arr: number[] = [1, 2, 3, 4, 5];
+let reversed: number[] = [];
+for (let i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr[i]);
+}
+console.log(reversed);
 
 
-let username: string = "Sapthagiri" ;
-// let message: string = `Hello ${username}, welcome to Playwright!`;
-console.log(username);
-// console.log(message);
